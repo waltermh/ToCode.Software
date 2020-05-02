ac25fd1a887e1caad603014dfb30da1a2cfdac51
Tim Purdum <tim@cedarrivertech.com>
2020-05-02 14:18:17 -0500

    Update to Chatbot 2000

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,58 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the MultiPlex 2000!");
+            Console.WriteLine("Who's bot would you like to talk to?");
+            var response = Console.ReadLine();
+            if (response.ToLower() == "makiah")
+            {
+                TalkToMakiah();
+            }
+            else if (response.ToLower() == "aylah")
+            {
+                TalkToAylah();
+            }
+            else if (response.ToLower() == "elliot")
+            {
+                TalkToElliot();
+            }
+            else if (response.ToLower() == "michael")
+            {
+                TalkToMichael();
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, no bots by the name {response} here!");	
+            }
+        }
+
+        void TalkToMakiah()
+        {
+            Console.WriteLine("Hello, this is Makiah's Bot.");
+            var bot = new MakiahBot();
+            bot.Main();
+        }
+
+        void TalkToAylah()
+        {
+            Console.WriteLine("Hello, this is Aylah's Bot.");
+            var bot = new AylahBot();
+            bot.Main();
+        }
+
+        void TalkToElliot()
+        {
+            Console.WriteLine("Hello, this is Elliot's Bot.");
+            var bot = new ElliotBot();
+            bot.Main();
+        }
+
+        void TalkToMichael()
+        {
+            Console.WriteLine("Hello, this is Michael's Bot.");
+            var bot = new MichaelBot();
+            bot.Main();
+        }
         }
     }
 }
